4e1a72d6776720cf635a57cb08e78ebae16c958a
Carlos (HPC) <carlos.franzreb@dfki.de>
2024-07-16 15:09:33 +0200

    Make target datafile optional if targets are given

--- knnvc/converter.py
+++ knnvc/converter.py
@@ -23,7 +23,8 @@ class Converter:
     def __init__(self, config: OmegaConf, device: str) -> None:
         """
         Initialize the converter. For the targets, it requires either a datafile
-        (config.target_df).
+        (config.target_df) to compute the target features, or a directory where
+        previously computed features are stored..
 
         You can also pass a directory with the WavLM features of the targets
         (config.target_feats), as dumped by a previous run. If this directory is not
@@ -32,39 +33,23 @@ class Converter:
 
         Args:
             config: the config object. Must contain `target_df`, which refers to a
-                datafile with the samples for the target selection algorithm.
+                datafile with the samples for the target selection algorithm, only if
+                there are no pre-computed features.
                 ! We assume that all samples of each speaker are consecutive.
             device: the device where the model should run.
         """
         self.config = config
         self.device = device
         self.target_selection = None  # initialized later (see init_target_selection)
-
-        # get the speaker IDs and paths of the targets
-        LOGGER.info("Loading target data")
-        self.target_files = list()
-        self.target_speakers = list()
-        current_spk = -1
-        current_samples = list()
-        target_df = os.path.join(config.exp_folder, "data", "targets.txt")
-        for line in open(target_df):
-            obj = json.loads(line)
-            spk = obj["speaker_id"]
-            if spk != current_spk and len(current_samples) > 0:
-                self.target_files.append(current_samples)
-                self.target_speakers.append(current_spk)
-                current_samples = list()
-                current_spk = spk
-            current_samples.append(obj["path"])
+        self.target_feats = list()
 
         # if possible, load the WavLM features of the targets
         target_feats_dir = config.get("target_feats", None)
-        self.target_feats = list()
         if target_feats_dir is not None:
             LOGGER.info(f"Loading target features from {target_feats_dir}")
-            for idx in range(len(self.target_speakers)):
+            for spk_idx in range(len(os.listdir(target_feats_dir))):
                 self.target_feats.append(
-                    torch.load(os.path.join(target_feats_dir, f"{idx}.pt"))
+                    torch.load(os.path.join(target_feats_dir, f"{spk_idx}.pt"))
                 )
             return
 
@@ -72,12 +57,13 @@ class Converter:
         LOGGER.info("Extracting target features")
         wavlm = setup_module(config.wavlm, device)
         dl = eval_dataloader(config.wavlm_dl, target_df, device)
-        self.target_feats = [None for _ in range(len(self.target_speakers))]
         for _, batch, data in dl:
             feats, feat_lengths = wavlm.run(batch).values()
             for idx in range(len(feats)):
                 spk = data[idx]["speaker_id"]
-                unpadded_feats = feats[idx, : feat_lengths[idx]]
+                if len(self.target_feats) == spk:
+                    self.target_feats.append(None)
+                unpadded_feats = feats[idx, : feat_lengths[idx]].to("cpu")
                 if self.target_feats[spk] is None:
                     self.target_feats[spk] = unpadded_feats
                 else:
@@ -112,11 +98,11 @@ class Converter:
             batch: a list with a tensor comprising spectrograms in first position.
         """
         # get the features, source and target speakers
-        feats = batch[self.config.input.feats]
-        n_feats = batch[self.config.input.n_feats]
-        source = batch[self.config.input.source]
+        feats = batch[self.config.input.feats].to("cpu")
+        n_feats = batch[self.config.input.n_feats].to("cpu")
+        source = batch[self.config.input.source].to("cpu")
         target_in = (
-            batch[self.config.input.target]
+            batch[self.config.input.target].to("cpu")
             if self.config.input.target in batch
             else None
         )
@@ -131,7 +117,7 @@ class Converter:
             )
         converted_batch = pad_sequence(converted_feats, batch_first=True)
 
-        return {"wavlm": converted_batch, "target": target, "n_feats": n_feats}
+        return {"wavlm": converted_batch.to(self.device), "target": target, "n_feats": n_feats}
 
     def convert_vecs(self, source_vecs: Tensor, target_vecs: Tensor) -> Tensor:
         """
